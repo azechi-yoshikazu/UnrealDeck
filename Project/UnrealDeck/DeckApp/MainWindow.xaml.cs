1108cd551764fc118b9143f6ae91c9b01fe4339e
azechi_yoshikazu_gs <azechi.yoshikazu@guildstudio.co.jp>
2023-01-25 11:11:54 +0900

    Created pane in main window, then placed button that create new workspace in the Workspace pane

--- Project/UnrealDeck/DeckApp/MainWindow.xaml.cs
+++ Project/UnrealDeck/DeckApp/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +23,11 @@ namespace DeckApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnAddWorkspaceButtonClicked(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Add workspace");
         }
     }
 }
