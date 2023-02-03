bd28361fc63e5f66ec5858fd8f7b531317adcf84
azechi_yoshikazu_gs <azechi.yoshikazu@guildstudio.co.jp>
2023-02-03 20:41:29 +0900

    Implemented view update

--- Project/UnrealDeck/DeckApp/UI/MainViewModel.cs
+++ Project/UnrealDeck/DeckApp/UI/MainViewModel.cs
@@ -1,24 +1,37 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
-using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
 
 namespace DeckApp.UI
 {
     public class MainViewModel : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
 
-        public List<Domain.Workspaces.Workspace> Workspaces { get; } = new List<Domain.Workspaces.Workspace>();
+        public ObservableCollection<Domain.Workspaces.Workspace> Workspaces { get; } = new ObservableCollection<Domain.Workspaces.Workspace>();
 
         public MainViewModel()
         {
             Domain.Workspaces.WorkspaceID ID = new Domain.Workspaces.WorkspaceID("ID");
             Domain.Workspaces.WorkspaceDirectory Directory = new Domain.Workspaces.WorkspaceDirectory($"Directory");
             Workspaces.Add(new Domain.Workspaces.Workspace(ID, Directory));
+        }
+
+        // TODO: Replace to ICommand
+        public void Add()
+        {
+            Domain.Workspaces.WorkspaceID ID = new Domain.Workspaces.WorkspaceID("ID");
+            Domain.Workspaces.WorkspaceDirectory Directory = new Domain.Workspaces.WorkspaceDirectory($"Directory");
+            Workspaces.Add(new Domain.Workspaces.Workspace(ID, Directory));
+
+            Debug.WriteLine($"Add({Workspaces.Count})");
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Workspaces"));
         }
     }
 }
--- Project/UnrealDeck/DeckApp/UI/MainWindow.xaml.cs
+++ Project/UnrealDeck/DeckApp/UI/MainWindow.xaml.cs
@@ -18,25 +18,8 @@ namespace DeckApp.UI
 
         private void OnAddWorkspaceButtonClicked(object sender, RoutedEventArgs e)
         {
-            //Debug.WriteLine("Add workspace");
-            //{
-            //    Domain.Workspaces.WorkspaceID ID = new Domain.Workspaces.WorkspaceID($"ID_{incrementalID}");
-            //    Domain.Workspaces.WorkspaceDirectory Directory = new Domain.Workspaces.WorkspaceDirectory($"Dir_{incrementalID}");
-            //    Domain.Workspaces.Workspace newWorkspace = new Domain.Workspaces.Workspace(ID, Directory);
-
-            //    var service = serviceProvider.GetRequiredService<Application.Workspaces.WorkspaceService>();
-            //    var result = service.Create(newWorkspace);
-
-            //    if (result.Item1)
-            //    {
-            //        foreach (var workspace in result.Item2)
-            //        {
-            //            Debug.WriteLine(workspace);
-            //        }
-            //    }
-
-            //    incrementalID++;
-            //}
+            MainViewModel viewmodel = (MainViewModel)DataContext;
+            viewmodel?.Add();
         }
     }
 }
